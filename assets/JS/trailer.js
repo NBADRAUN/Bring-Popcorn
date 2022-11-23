59d6795b9cc3a576aa7b13ba0022cf520dc1c2a2
Jelenarog <jelena.rogic90@gmail.com>
2022-11-22 22:46:13 -0700

    cleaning js

--- assets/JS/trailer.js
+++ assets/JS/trailer.js
@@ -1,6 +1,5 @@
 var fetchButton = $("#searchButton");
 var screenHolder = $("#holderForTrailer");
-//var test = $("#formContainer");
 
 
 var formHandler = function (event) {
@@ -13,38 +12,17 @@ var formHandler = function (event) {
   getTrailer();
 };
 
-
-
 //**User can search for movie of interest**//
 var pickMovie = function () {
   movieSelection = trailerSelection.value.trim();
   console.log(movieSelection);
-  if (movieSelection === null) {
-    //need to add condition to stop function if not input
-    console.log("no selection");
-    return;
-  }
 };
 
-
 var getTrailer = function () {
-    //Selects the card collection body
-    // var cardsContainer = $('#formContainer');
-    // cardsContainer.addClass('mx-2', 'mx-2');
-    
-    //var cardsContainer =$('<div>').addClass('col')
-    
-    //Gets rid of previous cards when selecting new genre
-    // if (cardsContainer.hasChildNodes()) {
-    // while (cardsContainer.firstChild) {
-    //     cardsContainer.removeChild(cardsContainer.firstChild);
-    //   }
-    // };
-
-
-var testName= 'Black Panther: Wakanda Forever';
-var testDescr=' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.a new path for the kingdom of Wakanda.'
-var testvideo= 'https://trailer.movieglu.com/297863_high_V2.mp4'
+  var testName = "Black Panther: Wakanda Forever";
+  var testDescr =
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.a new path for the kingdom of Wakanda.";
+  var testvideo = "https://trailer.movieglu.com/297863_high_V2.mp4";
 
   // var getMovieUrl = {
   //   url: "https://api-gate2.movieglu.com/filmsNowShowing/?n=10",
@@ -62,196 +40,62 @@ var testvideo= 'https://trailer.movieglu.com/297863_high_V2.mp4'
 
   // $.ajax(getMovieUrl).done(function (response) {
   //   console.log(response);
-    //for (let i = 0; i < response.films.length; i++) {
-      for (let i = 0; i < 5; i++) {
+  //for (let i = 0; i < response.films.length; i++) {
+  for (let i = 0; i < 5; i++) {
+    //console.log(response.films[i].film_name);
 
-      //console.log(response.films[i].film_name);
-        // //Creates information variables to plug into cards
-        // matchMovie = response.films[i].film_name;
-        // movieTrailer = response.films[i].film_trailer; ////NEED TO ADD TO HTML
-        // movieDescr = response.films[i].synopsis_long;
-        
-       // console.log(movieTrailer);
-     
+    // //Creates information variables to plug into cards
+    // matchMovie = response.films[i].film_name;
+    // movieTrailer = response.films[i].film_trailer; ////NEED TO ADD TO HTML
+    // movieDescr = response.films[i].synopsis_long;
+   // console.log(movieTrailer);
 
-         //Creates Card form container
-         var card = $('<div>');
-         card.addClass(['card', 'custom-card', 'text-center']);
-        // card.style.width = '15rem';
-        // card.style.border = '0.1rem solid black';
-
- //Creates video element
- var trailerVideo = $("<video>");
- trailerVideo.addClass(['col', 'mt-3','w-5']);
-
- ////******for styling purpose only************/
-trailerVideo.attr("src", testvideo);
- //trailerVideo.attr("src", movieTrailer);
- trailerVideo.attr("type", "video/mp4");
- trailerVideo.attr("controls", "autoplay");
-
-
-
-
-
- 
- //Creates Card body
-//  var cardBody = $('<div>');
-//  cardBody.addClass(['card-body', 'rounded' ]);
-
- //Creates title
- var descrTitle = $('<h5>').addClass(' text-center');
-    // cardTitle.addClass('card-title' );
-     
-
-////******for styling purpose only************/s
-descrTitle.text(testName);
-   //  cardTitle.text(matchMovie);
-
-
-//Creates list form
-    //  var ul = $('<ul>');
-    //       ul.addClass(['list-group', 'list-group-flush', 'mb-2']);
-
-  //Creates list elements
- var liDescr = $('<p>').addClass('rounded shadow-lg p-3 mb-5 bg-body rounded');
-  //liDescr.addClass ('list-group-item ');
-
-  ////******for styling purpose only************/s
-  liDescr.text(testDescr);
-// liDescr.text(movieDescr);
-
-
-    //Appends information into cards
-
-    // sizerWrap.append(trailerVideo);
-    // sizer.append(sizerWrap);
-
-var divRow = $('<div>').addClass('row');
-var divCon = $('<div>').addClass('container');
-var cardsContainer = $('<div>').addClass('col pt-3');
-   var test = $('<div>').addClass('col pt-5 ');
-   cardsContainer.append(trailerVideo);
+    //Creates video elements
+    var trailerVideo = $("<video>");
+    trailerVideo.addClass(["col", "mt-3", "w-5"]);
 
    
-   test.append(cardsContainer);
-    // card.append(cardTitle);
-    // card.append(liDescr);
-   // card.append(cardBody);
+    trailerVideo.attr("src", testvideo);//(movieTrailer)
+    trailerVideo.attr("type", "video/mp4");
+    trailerVideo.attr("controls", "autoplay");
 
-   test.append(descrTitle);
-   test.append(liDescr);
+    //Creates element title for each trailer and style
+    var descrTitle = $("<h5>").addClass(" text-center");
+    descrTitle.text(testName);// (matchMovie);
     
-    divRow.append(test);
-    divRow.append(cardsContainer);
+
+    var liDescr = $("<p>").addClass(  "rounded shadow-lg p-3 mb-5 bg-body rounded");
+    liDescr.text(testDescr);//(movieDescr)
+    var divRow = $("<div>").addClass("row");
+    var divCon = $("<div>").addClass("container");
+    var trailerContainer = $("<div>").addClass("col pt-3");
+    var textContainer = $("<div>").addClass("col pt-5 ");
+
+  //append to HTML
+    trailerContainer.append(trailerVideo);
+    textContainer.append(trailerContainer);
+    textContainer.append(descrTitle);
+    textContainer.append(liDescr);
+
+    divRow.append(textContainer);
+    divRow.append(trailerContainer);
     divCon.append(divRow);
-    var wrap= $('#wrapper');
+    var wrap = $("#wrapper");
     wrap.append(divCon);
 
-        // cardsContainer.append(card);
-    // cardsContainer.append (cardBody);
-   
 
 
-
-
-
-
-
-
-
-
-        // var trailerWrapper = $("<div>");
-        // trailerWrapper.addClass("card");
-        // var movieName = $("<h5>");
-        // movieName.addClass("card-title");
-        // movieName.text(matchMovie);
-        // var movieDescription = $("<p>");
-        // movieDescription.addClass("card-text");
-        // movieDescription.text(movieDescr);
-        // var outputBody = $("<div>");
-        // outputBody.addClass("card-body");
-       // var trailerVideo = $("<video>");
-        //trailerVideo.addClass("autoplay");
-       // var source = $("<src>");
-       // trailerVideo.attr("src", movieTrailer);
-       // trailerVideo.attr("type", "video/mp4");
-      //  // trailerVideo.attr("controls", "autoplay");
-
-      //   console.log(source);
-
-      //   outputBody.append(movieName);
-      //   outputBody.append(movieDescription);
-      //  // trailerWrapper.append(trailerVideo);
-      //   trailerWrapper.append(outputBody);
-      //   screenHolder.append(trailerWrapper);
-
-        // console.log(movieTrailer);
-        //console.log(movieDescr);
-        //console.log(matchMovie);
-      }
-      //    else if ( movieSelection !== response.films[i].film_name)
-      //     {
-      //        console.log('Movie you are looking for is not in our database');  //need to stop search****
-      //     }
-   // }
-//   });
- };
+  }
+  //    else if ( movieSelection !== response.films[i].film_name)
+  //     {
+  //        console.log('Movie you are looking for is not in our database');  //need to stop search****
+  //     }
+  // }
+  //   });
+};
 getTrailer();
 // fetchButton.on("submit", formHandler);
 // //Black Panther: Wakanda Forever
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // var getTrailer = function () {
 //   var getMovieUrl = {
